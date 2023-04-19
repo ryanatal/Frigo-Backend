77bc34a1783c0b7b3e2ff217274690438d5890c5
Ryan Atallah <ryanatallah02@gmail.com>
2023-04-19 10:53:28 +0300

    Backend Init

--- frontend/src/IngredientsSelectedContext.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export const IngredientsSelectedContext = React.createContext(
-    {
-        selectedIngredients: [],
-        setSelectedIngredients: () => { },
-    }
-);
