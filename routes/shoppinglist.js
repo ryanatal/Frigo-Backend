d6d397db623f3727f905e6b7d9652ca629e751b3
omar issa <oissa@omars-MacBook-Pro.local>
2023-04-24 18:35:35 +0300

    add to pantry from shoppinglist

--- routes/shoppinglist.js
+++ routes/shoppinglist.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { addItems, getItems } from '../controllers/shoppinglist.js';
+import { addItemToPantry, addItems, getItems } from '../controllers/shoppinglist.js';
 
 const router = express.Router();
 
 router.post('/add', addItems);
 router.get('/get', getItems);
+router.post('/addToPantry', addItemToPantry);
 
 export default router;
